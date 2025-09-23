1363a5dbeceeb5220059450ccd2aa2330fdd92f6
JITHENDRA-MOOLINTI <moolintijithendrareddy225@gmail.com>
2025-09-23 09:35:15 +0530

    promises change

--- promises/index.js
+++ promises/index.js
@@ -1,58 +1,58 @@
  const restName='Raju Gari Biryani';
-// function browse(restName,callback){
-//      setTimeout(()=>{
-//         console.log(`${restName} Restaurant is Found `);
-//         if(callback) callback();
-//      },2000)
-// }
-// function selectFood(restName,callback){
-//      setTimeout(()=>{
-//         console.log(`Food is Selected at ${restName}`);
-//         if(callback) callback();
-//      },1500)
-// }
-// function placeOrder(restName,callback){
-//      setTimeout(()=>{
-//         console.log(`Order is placed From ${restName}`);
-//         if(callback) callback();
-//      },2000)
-// }
+function browse(restName,callback){
+     setTimeout(()=>{
+        console.log(`${restName} Restaurant is Found `);
+        if(callback) callback();
+     },2000)
+}
+function selectFood(restName,callback){
+     setTimeout(()=>{
+        console.log(`Food is Selected at ${restName}`);
+        if(callback) callback();
+     },1500)
+}
+function placeOrder(restName,callback){
+     setTimeout(()=>{
+        console.log(`Order is placed From ${restName}`);
+        if(callback) callback();
+     },2000)
+}
 
-// function makePayment(restName,callback){
-//      setTimeout(()=>{
-//         console.log(`payment successful to  ${restName}`);
-//         if(callback) callback();
-//      },1000)
-// }
+function makePayment(restName,callback){
+     setTimeout(()=>{
+        console.log(`payment successful to  ${restName}`);
+        if(callback) callback();
+     },1000)
+}
 
-// function prepareFood(restName,callback){
-//      setTimeout(()=>{
-//         console.log(`Food Prepared From ${restName}`);
-//         if(callback) callback();
-//      },3000)
-// }
-// function deliver(restName,callback){
-//      setTimeout(()=>{
-//         console.log(`Order Deliver to Customer`);
-//         if(callback) callback();
-//      },3000)
+function prepareFood(restName,callback){
+     setTimeout(()=>{
+        console.log(`Food Prepared From ${restName}`);
+        if(callback) callback();
+     },3000)
+}
+function deliver(restName,callback){
+     setTimeout(()=>{
+        console.log(`Order Deliver to Customer`);
+        if(callback) callback();
+     },3000)
 
-// }
+}
 
 
-// browse(restName,()=>{
-//     selectFood(restName,()=>{
-//         placeOrder(restName,()=>{
-//             makePayment(restName,()=>{
-//                 prepareFood(restName,()=>{
-//                     deliver(restName,()=>{
-//                         console.log('deliver is successfull')
-//                     });
-//                 })
-//             })
-//         })
-//     })
-// })
+browse(restName,()=>{
+    selectFood(restName,()=>{
+        placeOrder(restName,()=>{
+            makePayment(restName,()=>{
+                prepareFood(restName,()=>{
+                    deliver(restName,()=>{
+                        console.log('deliver is successfull')
+                    });
+                })
+            })
+        })
+    })
+})
 
 
 function browse(restName){
